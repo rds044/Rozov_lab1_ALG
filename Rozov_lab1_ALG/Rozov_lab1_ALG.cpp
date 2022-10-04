0c428a405e2d9594c732298e40fb44ea454b8ea7
seps2 <seps2@DESKTOP-FN8RBR6>
2022-10-04 20:39:48 +0300

    Добавьте файлы проекта.

---
+++ Rozov_lab1_ALG/Rozov_lab1_ALG.cpp
@@ -0,0 +1,10 @@
+﻿// Rozov_lab1_ALG.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
